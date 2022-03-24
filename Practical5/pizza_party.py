9cf1ccfe78f90845226454fdff899615a48fcf2f
ljc <2541958582@qq.com>
2022-03-24 23:07:04 +0800

    Practical

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,8 @@
+#1. use for Loops to ensure the continous output of a sequence. The range should be 1-64
+for i in range(1,65):
+#2.define a variable p and caculate p using the formula given.
+#use i  as the basic variable to p to make it a sequence output.
+    p=(i**2+i+2)/2
+    print(i)
+    print(p)
+#Both i and p printed to make it more clear.
